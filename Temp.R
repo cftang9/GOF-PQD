2caedd4052ce971becd6fc514b32d8579ac6cd7e
cftang9 <45774851+cftang9@users.noreply.github.com>
2025-07-17 04:50:59 -0500

    Update Temp.R

--- Temp.R
+++ Temp.R
@@ -1,4 +1,5 @@
 # Source the main function from PQD_GOF.R online
+rm(list=ls(all=TRUE))
 source("https://raw.githubusercontent.com/cftang9/GOF-PQD/refs/heads/main/PQD_GOF.R")
 set.seed(10000)
 library(copula)
